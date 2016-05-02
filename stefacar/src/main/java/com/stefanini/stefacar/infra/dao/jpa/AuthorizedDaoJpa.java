ffc42d355db33c98210dce77a46741b3be4e431d
rluisb <ricardo220893@gmail.com>
2016-05-02 15:41:17 -0300

    Corrigida a historia de authorized

--- stefacar/src/main/java/com/stefanini/stefacar/infra/dao/jpa/AuthorizedDaoJpa.java
+++ stefacar/src/main/java/com/stefanini/stefacar/infra/dao/jpa/AuthorizedDaoJpa.java
@@ -1,5 +1,14 @@
 package com.stefanini.stefacar.infra.dao.jpa;
 
-public class AuthorizedDaoJpa {
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
+import com.stefanini.stefacar.model.domain.Authorized;
+
+public class AuthorizedDaoJpa extends AbstractDaoJpa<Integer, Authorized>{
+
+	@Inject
+	public AuthorizedDaoJpa(EntityManager entityManager) {
+		super(entityManager);
+	}
 }
