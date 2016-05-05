2511847c2a124741bec82c21967ab21a81231d18
Pedro Vitor Meira <pv.ciria@gmail.com>
2016-05-05 13:20:37 -0300

    mexendo nas telas de cadastro

--- stefacar/src/main/java/com/stefanini/stefacar/model/domain/Authorized.java
+++ stefacar/src/main/java/com/stefanini/stefacar/model/domain/Authorized.java
@@ -1,48 +1,14 @@
 package com.stefanini.stefacar.model.domain;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
+@SuppressWarnings("serial")
 @Entity
-public class Authorized {
+public class Authorized extends AbstractPerson {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idAuthorized;
-	@Column(nullable = false)
-	protected String authorizedName;
-
-	// @OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
-	// @JoinColumn(nullable = false)
-	// protected ContactList contactList;
-
-	// @OneToOne(cascade = CascadeType.REFRESH)
-	// @JoinColumn(name = "idAddress", nullable = false)
-	// protected Address authorizedAdress;
 	@Column
 	private String AuthorizedCnpj;
-
-	public int getIdAuthorized() {
-		return idAuthorized;
-	}
-
-	public void setIdAuthorized(int idAuthorized) {
-		this.idAuthorized = idAuthorized;
-	}
-
-	public String getAuthorizedName() {
-		return authorizedName;
-	}
-
-	public void setAuthorizedName(String authorizedName) {
-		this.authorizedName = authorizedName;
-	}
 
 	public String getAuthorizedCnpj() {
 		return AuthorizedCnpj;
@@ -50,40 +16,6 @@ public class Authorized {
 
 	public void setAuthorizedCnpj(String authorizedCnpj) {
 		AuthorizedCnpj = authorizedCnpj;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((AuthorizedCnpj == null) ? 0 : AuthorizedCnpj.hashCode());
-		result = prime * result + ((authorizedName == null) ? 0 : authorizedName.hashCode());
-		result = prime * result + idAuthorized;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Authorized other = (Authorized) obj;
-		if (AuthorizedCnpj == null) {
-			if (other.AuthorizedCnpj != null)
-				return false;
-		} else if (!AuthorizedCnpj.equals(other.AuthorizedCnpj))
-			return false;
-		if (authorizedName == null) {
-			if (other.authorizedName != null)
-				return false;
-		} else if (!authorizedName.equals(other.authorizedName))
-			return false;
-		if (idAuthorized != other.idAuthorized)
-			return false;
-		return true;
 	}
 
 }
