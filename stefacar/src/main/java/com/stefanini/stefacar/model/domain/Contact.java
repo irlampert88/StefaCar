359cdab59595383cce413d04578d429f22f9451f
ThiagoKakoSilveira <kako_blackbelt@hotmail.com>
2016-05-06 15:01:42 -0300

    Segue correção da classe contactList com a anotação de Trasient para informar que a entidade não precisa ser persistida

--- stefacar/src/main/java/com/stefanini/stefacar/model/domain/Contact.java
+++ stefacar/src/main/java/com/stefanini/stefacar/model/domain/Contact.java
@@ -1,18 +1,29 @@
 package com.stefanini.stefacar.model.domain;
 
-//@Entity
-// @Inheritance(strategy = InheritanceType.JOINED)
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+ @Inheritance(strategy = InheritanceType.JOINED)
 public class Contact {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idContact;
-//	@Column
+	@Column
 	private String type;
-//	@Column
+	@Column
 	private String value;
 
-//	@ManyToOne(cascade = CascadeType.REFRESH)
-//	@JoinColumn(name = "listaDeContatosID")
+	@ManyToOne(cascade = CascadeType.REFRESH)
+	@JoinColumn(name = "listaDeContatosID")
 	private ContactList listContacts;
 
 	public Contact() {
