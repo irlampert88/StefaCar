f47a975d91354b082be2a81007cb6c93338c9b10
rluisb <ricardo220893@gmail.com>
2016-05-06 14:20:46 -0300

    Entidade brand e model criadas

--- stefacar/src/main/java/com/stefanini/stefacar/model/domain/Brand.java
+++ stefacar/src/main/java/com/stefanini/stefacar/model/domain/Brand.java
@@ -1,19 +1,26 @@
 package com.stefanini.stefacar.model.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
-@Entity
-public class Foo {
+public class Brand {
+	
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
+	private Integer idMarca;
 	@Column(nullable=false)
 	private String name;
+	@OneToMany(cascade=CascadeType.REFRESH)	
+	@JoinColumn(name="idModel")
+	private List<Model> models;
+	
 
 }
