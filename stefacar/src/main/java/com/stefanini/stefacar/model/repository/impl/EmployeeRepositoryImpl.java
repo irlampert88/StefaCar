a906c82267709dd0ed39792ea1254e8bbee2475c
jutorres <torresjujuliana@gmail.com>
2016-05-19 11:27:31 -0300

    Refatorando a serviceEmployee

--- stefacar/src/main/java/com/stefanini/stefacar/model/repository/impl/EmployeeRepositoryImpl.java
+++ stefacar/src/main/java/com/stefanini/stefacar/model/repository/impl/EmployeeRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.stefanini.stefacar.model.repository.impl;
 
-import java.util.List;
 
 import com.stefanini.stefacar.model.domain.Employee;
 
--- stefacar/src/main/java/com/stefanini/stefacar/model/service/impl/EmployeeServiceImpl.java
+++ stefacar/src/main/java/com/stefanini/stefacar/model/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,41 @@
 package com.stefanini.stefacar.model.service.impl;
 
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import com.stefanini.stefacar.infra.dao.transactional.Transactional;
 import com.stefanini.stefacar.model.domain.Employee;
 import com.stefanini.stefacar.model.repository.impl.EmployeeRepositoryImpl;
 
-public class EmployeeServiceImpl extends AbstractService<Employee> {
+public class EmployeeServiceImpl {
 
 	@Inject
-	public EmployeeServiceImpl(EmployeeRepositoryImpl repository) {
-		super(Employee.class, repository);
+	private EmployeeRepositoryImpl repository;
+	
+	public Iterable<Employee> listAll(Employee employee) {
+		return Collections.emptyList();
+	}
+	
+	@Transactional
+	public void save(Employee employee){
+		if (employee.getId() == null) {
+			repository.insert(employee);
+		} else {
+			repository.update(employee);
+		} 
+	}
+	
+	@Transactional
+	public void delete(Employee employee){
+		repository.delete(employee);
+	}
+	
+	@Transactional
+	public List<Employee> loadAllEmployeeFromDataBase() {
+		return repository.listAllRecords();		
 	}
 	
 }
