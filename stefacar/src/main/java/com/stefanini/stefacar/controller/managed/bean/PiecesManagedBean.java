07c3514151db5a3502b09248a5bffca7123a8362
Pedro Vitor Meira <pv.ciria@gmail.com>
2016-05-24 16:20:02 -0300

    Criando CRUD peças

---
+++ stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/PiecesManagedBean.java
@@ -0,0 +1,59 @@
+package com.stefanini.stefacar.controller.managed.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import com.stefanini.stefacar.controller.converter.MessengerSystem;
+import com.stefanini.stefacar.model.domain.Pieces;
+import com.stefanini.stefacar.model.service.impl.PiecesServiceImpl;
+
+@ManagedBean
+@ViewScoped
+public class PiecesManagedBean {
+	private Pieces pieces;
+	private List<Pieces> dataList;
+
+	@Inject
+	protected PiecesServiceImpl service;
+
+	@PostConstruct
+	public void init() {
+		pieces = new Pieces();
+		dataList = service.loadAllPiecesFromDataBase();
+	}
+
+	public void save() {
+		service.save(getPieces());
+		MessengerSystem.notificaInformacao("Parabens!", "Cadastro da Peça salvo com sucesso");
+		clean();
+	}
+
+	public void delete(Pieces pieces) {
+		service.delete(pieces);
+		MessengerSystem.notificaInformacao("Parabens!", "Cadastro da Peça excluido com sucesso!");
+	}
+
+	public void setList(List<Pieces> dataList) {
+		this.dataList = dataList;
+	}
+
+	public List<Pieces> getDataList() {
+		return dataList;
+	}
+
+	public Pieces getPieces() {
+		return pieces;
+	}
+
+	public void setPieces(Pieces pieces) {
+		this.pieces = pieces;
+	}
+
+	public void clean() {
+		setPieces(new Pieces());
+	}
+}
