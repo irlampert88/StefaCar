4b80b511bcc48c0db59c46ee9069eb4061a79669
Carlos R. P. Soares <carecarestinga@gmail.com>
2016-05-27 13:46:12 -0300

    Ajustes nas Mensagens

--- stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/OutfitterManagedBean.java
+++ stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/OutfitterManagedBean.java
@@ -30,13 +30,13 @@ public class OutfitterManagedBean {
 
 	public void save() {
 		service.save(getOutfitter());
-		MessengerSystem.notificaInformacao("Parabens!", "Cadastro de Client salvo com sucesso");
+		MessengerSystem.notificaInformacao("Parabens!", "Cadastro de Fornecedor salvo com sucesso");
 		clean();
 	}
 
 	public void delete(Outfitter outfitter) {
 		service.delete(outfitter);
-		MessengerSystem.notificaInformacao("Parabens!", "Cadastro de Client excluido com sucesso!");
+		MessengerSystem.notificaInformacao("Parabens!", "Cadastro de Fornecedor excluido com sucesso!");
 	}
 
 	public void setList(List<Outfitter> dataList) {
