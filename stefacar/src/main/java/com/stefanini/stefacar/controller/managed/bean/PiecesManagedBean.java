f896e8358a76b6ba3e4b8c3ca5a5387acb9d9133
Pedro Vitor Meira <pv.ciria@gmail.com>
2016-05-31 15:54:39 -0300

    Trocando one menu por POPUP

--- stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/PiecesManagedBean.java
+++ stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/PiecesManagedBean.java
@@ -8,7 +8,13 @@ import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import com.stefanini.stefacar.controller.converter.MessengerSystem;
+import com.stefanini.stefacar.model.domain.Manufacturer;
+import com.stefanini.stefacar.model.domain.ModelCar;
+import com.stefanini.stefacar.model.domain.Outfitter;
 import com.stefanini.stefacar.model.domain.Pieces;
+import com.stefanini.stefacar.model.repository.impl.ManufacturerRepository;
+import com.stefanini.stefacar.model.repository.impl.ModelCarRepositoryImpl;
+import com.stefanini.stefacar.model.repository.impl.OutfitterRepository;
 import com.stefanini.stefacar.model.service.impl.PiecesServiceImpl;
 
 @ManagedBean
@@ -20,8 +26,24 @@ public class PiecesManagedBean {
 	@Inject
 	protected PiecesServiceImpl service;
 
+	@Inject
+	private OutfitterRepository outfitterRepository;
+
+	@Inject
+	private ManufacturerRepository manufacturerRepository;
+
+	@Inject
+	private ModelCarRepositoryImpl modelRepository;
+
+	private List<Outfitter> listOfOutfitter;
+	private List<Manufacturer> listOfManufacturer;
+	private List<ModelCar> listOfmodel;
+
 	@PostConstruct
 	public void init() {
+		listOfOutfitter = outfitterRepository.listAllRecords();
+		listOfManufacturer = manufacturerRepository.listAllRecords();
+		listOfmodel = modelRepository.listAllRecords();
 		pieces = new Pieces();
 		dataList = service.loadAllPiecesFromDataBase();
 	}
@@ -43,6 +65,30 @@ public class PiecesManagedBean {
 
 	public List<Pieces> getDataList() {
 		return dataList;
+	}
+
+	public List<Outfitter> getListOfOutfitter() {
+		return listOfOutfitter;
+	}
+
+	public void setListOfOutfitter(List<Outfitter> listOfOutfitter) {
+		this.listOfOutfitter = listOfOutfitter;
+	}
+
+	public List<Manufacturer> getListOfManufacturer() {
+		return listOfManufacturer;
+	}
+
+	public void setListOfManufacturer(List<Manufacturer> listOfManufacturer) {
+		this.listOfManufacturer = listOfManufacturer;
+	}
+
+	public List<ModelCar> getListOfmodel() {
+		return listOfmodel;
+	}
+
+	public void setListOfmodel(List<ModelCar> listOfmodel) {
+		this.listOfmodel = listOfmodel;
 	}
 
 	public Pieces getPieces() {
