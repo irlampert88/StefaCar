a2dbf77445d707f8bb240f1d3f8a7a08972e860a
jutorres <torresjujuliana@gmail.com>
2016-06-02 16:55:17 -0300

    Arrumando dropdown

--- stefacar/src/main/webapp/resources/js/default.js
+++ stefacar/src/main/webapp/resources/js/default.js
@@ -1,28 +1,17 @@
-(function(window){
+$(".dropdown li").on('mouseenter mouseleave', function (e) {
 
-	// get vars
-	var searchEl = document.querySelector("#input");
-	var labelEl = document.querySelector("#label");
+        // Get the computed style of the body element
+        var cStyle = document.body.currentStyle||window.getComputedStyle(document.body, "");
 
-	// register clicks and toggle classes
-	labelEl.addEventListener("click",function(){
-		if (classie.has(searchEl,"focus")) {
-			classie.remove(searchEl,"focus");
-			classie.remove(labelEl,"active");
-		} else {
-			classie.add(searchEl,"focus");
-			classie.add(labelEl,"active");
-		}
-	});
-
-	// register clicks outisde search box, and toggle correct classes
-	document.addEventListener("click",function(e){
-		var clickedID = e.target.id;
-		if (clickedID != "search-terms" && clickedID != "search-label") {
-			if (classie.has(searchEl,"focus")) {
-				classie.remove(searchEl,"focus");
-				classie.remove(labelEl,"active");
-			}
-		}
-	});
-}(window));
+        // Check the overflow and overflowY properties for "auto" and "visible" values
+        var hasVScroll = cStyle.overflow == "visible" 
+                 || cStyle.overflowY == "visible"
+                 || (hasVScroll && cStyle.overflow == "auto")
+                 || (hasVScroll && cStyle.overflowY == "auto");
+                 
+        if (hasVScroll) {
+            $(this).addClass('edge');
+        } else {
+            $(this).removeClass('edge');
+        }
+    });
