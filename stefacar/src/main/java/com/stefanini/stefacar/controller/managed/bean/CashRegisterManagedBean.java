b34242ab0aaae0add503b12a74fc816c110c8068
ThiagoKakoSilveira <kako_blackbelt@hotmail.com>
2016-06-07 13:10:00 -0300

    Correcao do backEnd de caixa com comentarios substituidos por codigos mas ainda falta testar

--- stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/CashRegisterManagedBean.java
+++ stefacar/src/main/java/com/stefanini/stefacar/controller/managed/bean/CashRegisterManagedBean.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ValueChangeEvent;
 import javax.inject.Inject;
 
 import com.stefanini.stefacar.controller.warehouse.TypeOfPayment;
@@ -12,6 +13,7 @@ import com.stefanini.stefacar.model.domain.CashRegister;
 import com.stefanini.stefacar.model.domain.Sale;
 import com.stefanini.stefacar.model.repository.impl.CashRegisterRepositoryImpl;
 import com.stefanini.stefacar.model.repository.impl.SaleRepositoryImpl;
+import com.stefanini.stefacar.model.service.impl.CashRegisterServiceImpl;
 
 @ManagedBean
 @ViewScoped
@@ -27,25 +29,112 @@ public class CashRegisterManagedBean implements Serializable {
 	
 	@Inject
 	SaleRepositoryImpl repositorySale;
+	
+	@Inject
+	CashRegisterServiceImpl service;
 		
 	private CashRegister cashregister;
 	private Sale saleSelected;
+	private double discountedOrInterest;
+	
 	@PostConstruct
 	public void init(){
 		cashregister = new CashRegister();
+		saleSelected = new Sale();
 		cashregister.setSales(repositorySale.listAllRecordsByActive());
 	}
 	
 	public void closeSales(Sale sale){
-		sale.setStatus(false);//venda finalizada		
+		sale.setStatus(false);//venda finalizada	
+		service.closeSale(sale);
 	}
 	
-	public void selectSale(Sale sale){
-		saleSelected = sale;
+	public void calculatesDiscount(ValueChangeEvent event){	
+		String payment = event.getNewValue().toString();
+		if(payment.equals("1") || payment.equals("3")){
+			discountedOrInterest = saleSelected.getCar().getPrice() * 0.9;
+			calculatesTransshipment();
+		}
+		if(payment.equals("2")){
+			discountedOrInterest = saleSelected.getCar().getPrice() * 1.3;
+			calculatesTransshipment();
+		}
+		if(payment.equals("4")){
+			double valorTotal=0;
+			double parcela = saleSelected.getCar().getPrice()/24;
+			for (int i = 0; i < 23; i++) {
+				valorTotal += parcela * 1.3;
+			}
+			discountedOrInterest = valorTotal;
+			calculatesTransshipment();
+		}
+		if(payment.equals("5")){
+			double valorTotal=0;
+			double parcela = saleSelected.getCar().getPrice()/36;
+			for (int i = 0; i < 35; i++) {
+				valorTotal += parcela * 1.3;
+			}
+			discountedOrInterest = valorTotal;
+			calculatesTransshipment();
+		}
+		if(payment.equals("6")){
+			double valorTotal=0;
+			double parcela = saleSelected.getCar().getPrice()/48;
+			for (int i = 0; i < 47; i++) {
+				valorTotal += parcela * 1.3;
+			}
+			discountedOrInterest = valorTotal;
+			calculatesTransshipment();
+		}
 	}
-		
+	
+	private void calculatesTransshipment(){
+		cashregister.setTransshipment(cashregister.getAmountPaid()-discountedOrInterest);						
+	}
+	
+//	public void calculatesDiscount(double value, ValueChangeEvent event){	
+//		String payment = event.getNewValue().toString();
+//		if(payment.equals("1") || payment.equals("3")){
+//			discountedOrInterest = value * 0.9; 
+//		}
+//		if(payment.equals("2")){
+//			discountedOrInterest = value * 1.3;			 
+//		}
+//		if(payment.equals("4")){
+//			double valorTotal=0;
+//			double parcela = value/24;
+//			for (int i = 0; i < 23; i++) {
+//				valorTotal += parcela * 1.3;
+//			}
+//			discountedOrInterest = valorTotal;		
+//		}
+//		if(payment.equals("5")){
+//			double valorTotal=0;
+//			double parcela = value/36;
+//			for (int i = 0; i < 35; i++) {
+//				valorTotal += parcela * 1.3;
+//			}
+//			discountedOrInterest = valorTotal;						
+//		}
+//		if(payment.equals("6")){
+//			double valorTotal=0;
+//			double parcela = value/48;
+//			for (int i = 0; i < 47; i++) {
+//				valorTotal += parcela * 1.3;
+//			}
+//			discountedOrInterest = valorTotal;			
+//		}
+//	}		
 	//GETERS & SETERS
 	
+	public double getDiscountedValue() {
+		return discountedOrInterest;
+	}
+
+	public void setDiscountedValue(double discountedValue) {
+		this.discountedOrInterest = discountedValue;
+	}
+
 	public CashRegister getCashregister() {
 		return cashregister;
 	}
--- stefacar/src/main/java/com/stefanini/stefacar/controller/warehouse/TypeOfPayment.java
+++ stefacar/src/main/java/com/stefanini/stefacar/controller/warehouse/TypeOfPayment.java
@@ -1,10 +1,12 @@
 package com.stefanini.stefacar.controller.warehouse;
 
 public enum TypeOfPayment {
-	A_VISTA(1, "à vista"),
-	CREDITO(2, "cartão de crédito"),
+	A_VISTA(1, "dinheiro"),
+	CREDITO(2, "cartão de crédito 3x"),
 	DEBITO(3, "cartão de débito"),
-	BOLETO(4, "boleto");
+	BOLETO24(4, "boleto 24X"),
+	BOLETO36(5, "boleto 36X"),
+	BOLETO48(6, "boleto 48X");
 	
 	private Integer id;
 	private String label;
