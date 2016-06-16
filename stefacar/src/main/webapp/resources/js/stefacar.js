623e61710a17959fc035362b3db622f31f10f9f9
rluisb <ricardo220893@gmail.com>
2016-06-16 15:29:20 -0300

    Atualizando layout, tudo funcionando menos o editar

--- stefacar/src/main/webapp/resources/js/stefacar.js
+++ stefacar/src/main/webapp/resources/js/stefacar.js
@@ -1,31 +1,28 @@
 $(document).ready(function(){
-    $("#table").load("/stefacar/pages/default/default.xhtml");
+	carrega("table", "/stefacar/pages/default/default.xhtml");
 });	
 
-function carrega(urlTable, urlForm){
-	$('#table').load(urlTable);
-	$('#form').load(urlForm);
-}
-
-function carregaModal(urlModal){
-	$('#modal').load(urlModal);
+function carregaUrl(urlTable, urlForm){
+	$("#table").load(urlTable);
+	$("#form").load(urlForm);
 }
 
 function redireciona(url){
 	$(location).attr('href', url);
 }
 	
-function mostraFormEscondeTable(){
-        $("#table").hide();
-        $("#form").show();
+function mostraFormEscondeTable(div, url){
+        $("#table").hide("drop");
+        carrega(div, url);
+        $("#form").show("drop");
 }
 
-function mostraTablaEscondeForm(urlTable){
-        $("#form").hide();
-        $('#table').load(urlTable);
-        $("#table").show();
+function mostraTablaEscondeForm(div, url){
+		$("#form").hide("drop");
+		carrega(div, url);
+        $("#table").show("drop");
 }
 
-function recarrega(url){
-	$(location).attr('href', url);
+function carrega(div, url){
+	$("#" + div).load(url);
 }
